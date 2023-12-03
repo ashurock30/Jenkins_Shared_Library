cc3ff8ddcad0703a31bad491bb957e07ce347685
unknown <ashutosh@xyz.com>
2023-12-03 15:58:07 +0530

    Updated Library

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,7 +1,7 @@
 def call(String project, String ImageTag, String hubUser){
     
     sh """   
-     trivy image ${hubUser}/${project}:latest > scan.txt
+     /root/.docker/cli-plugins/docker-scout quickview ${hubUser}/${project}:latest > scan.txt
      cat scan.txt
     """
 }
