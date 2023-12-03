a33e848a1f014aabd92115a9c5c81b264711549e
unknown <ashutosh@xyz.com>
2023-12-03 12:32:10 +0530

    Updated Library

--- vars/mvnIntegrationTest.groovy
+++ vars/mvnIntegrationTest.groovy
@@ -1,5 +1,5 @@
 def call(){
     withMaven(globalMavenSettingsConfig: '', jdk: 'JDK11', maven: 'Maven-3.9.5', mavenSettingsConfig: '', traceability: true) {
-        sh 'mvn verify -DskipUnitTests'
+        sh 'mvn verify'
     }
 }
