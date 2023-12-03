2d2daa0ef1a1f8cb0f531236230379681d8c9503
unknown <ashutosh@xyz.com>
2023-12-03 12:42:38 +0530

    Updated Library

--- vars/QualityGateStatus.groovy
+++ vars/QualityGateStatus.groovy
@@ -1,9 +1,7 @@
 
 
 def call(credentialsId){
-
-waitForQualityGate abortPipeline: false, credentialsId: credentialsId
-
+    waitForQualityGate abortPipeline: false, credentialsId: credentialsId
 }
 
 
--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,6 +1,7 @@
 def call(credentialsId){
 
-    withSonarQubeEnv(credentialsId: credentialsId) {
-         sh 'mvn clean package sonar:sonar'
+    withSonarQubeEnv(credentialsId: credentialsId, installationName:'SonarQube') {
+        withMaven(globalMavenSettingsConfig: '', jdk: 'JDK11', maven: 'Maven-3.9.5', mavenSettingsConfig: '', traceability: true) { 
+            sh 'mvn clean package sonar:sonar -Dsonar.projectKey=org.springframework.boot:spring-boot-starter-parent -Dsonar.projectName="Sonar-Docker-Test-App"' 
     }
 }
