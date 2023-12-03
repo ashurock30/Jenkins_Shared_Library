6e3ebee4d109137f6dce69b8052da7ec70f167b8
unknown <ashutosh@xyz.com>
2023-12-03 12:46:12 +0530

    Updated Library

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -3,5 +3,6 @@ def call(credentialsId){
     withSonarQubeEnv(credentialsId: credentialsId, installationName:'SonarQube') {
         withMaven(globalMavenSettingsConfig: '', jdk: 'JDK11', maven: 'Maven-3.9.5', mavenSettingsConfig: '', traceability: true) { 
             sh 'mvn clean package sonar:sonar -Dsonar.projectKey=org.springframework.boot:spring-boot-starter-parent -Dsonar.projectName="Sonar-Docker-Test-App"' 
+        }
     }
 }
