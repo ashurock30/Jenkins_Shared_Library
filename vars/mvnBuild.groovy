a9f82bfa8093a88cf5cf3b04f57b264d7d223910
unknown <ashutosh@xyz.com>
2023-12-03 12:26:07 +0530

    Updated Library

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,3 +1,5 @@
 def call(){
-    sh 'mvn clean install'
+    withMaven(globalMavenSettingsConfig: '', jdk: 'JDK11', maven: 'Maven-3.9.5', mavenSettingsConfig: '', traceability: true) {
+        sh 'mvn clean install'
+    }
 }
--- vars/mvnIntegrationTest.groovy
+++ vars/mvnIntegrationTest.groovy
@@ -1,3 +1,5 @@
 def call(){
-    sh 'mvn verify -DskipUnitTests'
+    withMaven(globalMavenSettingsConfig: '', jdk: 'JDK11', maven: 'Maven-3.9.5', mavenSettingsConfig: '', traceability: true) {
+        sh 'mvn verify -DskipUnitTests'
+    }
 }
