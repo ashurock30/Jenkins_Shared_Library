56eb7a049a35790659730ee5859c30a57ee9bbe5
unknown <ashutosh@xyz.com>
2023-12-03 12:11:39 +0530

    Updated Library

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,8 @@
-def call(Map stageParams) {
+def call(String branch_name) {
  
     checkout([
         $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
+        branches: [[name:  ${branch_name} ]],
+        userRemoteConfigs: [[ url: "https://github.com/ashurock30/DevOps_Project_Docker.git" ]]
     ])
   }
