adb2b093cef2092a99eb5a9b79b9162fb9cf9538
unknown <ashutosh@xyz.com>
2023-12-03 12:16:25 +0530

    Updated Library

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -2,7 +2,7 @@ def call(String branch_name) {
  
     checkout([
         $class: 'GitSCM',
-        branches: [[name:  ${branch_name} ]],
+        branches: [[name:  "${branch_name}" ]],
         userRemoteConfigs: [[ url: "https://github.com/ashurock30/DevOps_Project_Docker.git" ]]
     ])
   }
